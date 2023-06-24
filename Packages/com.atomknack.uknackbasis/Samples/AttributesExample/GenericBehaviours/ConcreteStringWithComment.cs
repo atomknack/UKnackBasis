e0f7fe84c410dfcd2a281bdc7d49f1681148457a
atomknack <110350925+atomknack@users.noreply.github.com>
2023-06-24 18:57:03 +0400

    feat: effrotlessuielemen classes are now implement ilayoutdependant
    Nothing of concrete types will break, but
    BREAKING CHANGE: this changes preconcrete ui types sufficiently enough to increase major version number
    BREAKING CHANGE: samples will be now removed and available in their own separate repository: https://github.com/atomknack/UKnackExampleProjects in project: https://github.com/atomknack/UKnackExampleProjects/tree/main/BasisSamples

--- Packages/com.atomknack.uknackbasis/Samples/AttributesExample/GenericBehaviours/ConcreteStringWithComment.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ConcreteStringWithComment : GenericBehaviour<string>
-{
-    [SerializeField] public string _comment;
-}

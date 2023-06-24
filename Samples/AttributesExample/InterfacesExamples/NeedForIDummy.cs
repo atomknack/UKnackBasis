ca081d1c018b732c1ed5001dfc1a9ac3d4b82ead
atomknack <110350925+atomknack@users.noreply.github.com>
2023-06-24 18:57:03 +0400

    feat: effrotlessuielemen classes are now implement ilayoutdependant
    Nothing of concrete types will break, but
    BREAKING CHANGE: this changes preconcrete ui types sufficiently enough to increase major version number
    BREAKING CHANGE: samples will be now removed and available in their own separate repository: https://github.com/atomknack/UKnackExampleProjects in project: https://github.com/atomknack/UKnackExampleProjects/tree/main/BasisSamples

--- Samples/AttributesExample/InterfacesExamples/NeedForIDummy.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UKnack.Attributes;
-using UnityEngine;
-
-public class NeedForIDummy : MonoBehaviour
-{
-    [SerializeReference][ValidReference(typeof(IDummy))] public MonoBehaviour dummy;
-}
--- Samples/AttributesExample/MarkAsColor/Editor/MarkNullAsPurpleAttributeDrawer.cs
+++
@@ -1,9 +0,0 @@
-﻿using UKnack.Attributes.KnackAttributeDrawers;
-using UnityEditor;
-#if UNITY_EDITOR
-[CustomPropertyDrawer(typeof(MarkNullAsPurpleAttribute))]
-public class MarkNullAsPurpleAttributeDrawer : MarkNullAsColorAttributeDrawer
-{
-
-}
-#endif

aaba56e3d4da92f3ea27aacf56baffb12a596064
atomknack <110350925+atomknack@users.noreply.github.com>
2023-06-26 16:17:02 +0400

    fix: assemblyinfo update

---
+++ UKnackBasisConcrete/AssemblyInfo/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UKnackEditorTools")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UKnackPro")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UKnackProConcrete")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UserAssemblyFriendOfUKnack")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UserAssemblyFriendOfUKnackCsFiles")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UserAssemblyFriendOfUKnackConcrete")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UKnackTests")]
+
